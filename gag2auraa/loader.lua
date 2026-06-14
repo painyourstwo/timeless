011573e8f6879f72566fdeb77c1bd73f2b6e5bba
pain <145183274+painyourstwo@users.noreply.github.com>
2026-06-14 16:59:05 +0530

    release script in development

---
+++ gag2auraa/loader.lua
@@ -0,0 +1,1 @@
+local s,e=pcall(funtion() loadstring(game:HttpGet("https://raw.githubusercontent.com/painyourstwo/timeless/refs/heads/main/gag2auraa/script.lua"))() end) if not s then print(e) setclipboard(e) end 
